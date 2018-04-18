8e8384617ef8c759cc75178539efc19e7646b4c0
Justin Chadwell <jedevc@gmail.com>
2018-04-18 21:59:39 +0545

    Add docstrings to many methods to explain functionality

--- dfsh.py
+++ dfsh.py
@@ -11,6 +11,10 @@ def main():
     sh.run()
 
 class Shell:
+    '''
+    The main shell class.
+    '''
+
     def __init__(self):
         self.builtins = {
             'exit': self._builtin_exit,
@@ -19,6 +23,10 @@ class Shell:
         }
 
     def run(self):
+        '''
+        Run the shell.
+        '''
+
         while True:
             try:
                 line = self.readline()
@@ -27,16 +35,28 @@ class Shell:
                 sys.exit(0)
 
     def readline(self):
+        '''
+        Read a command from stdin to execute.
+
+        Returns:
+            A raw string read from stdin.
+        '''
+
         return input('$ ')
 
-    def execute(self, line):
-        tokens = Tokenizer(line)
+    def execute(self, raw):
+        '''
+        Execute a command in the form of a raw string.
+        '''
+
+        tokens = Tokenizer(raw)
 
         parser = Parser(tokens)
         root = parser.parse()
         root.execute(self.builtins)
         root.wait()
 
+    # various shell builtins
     def _builtin_exit(self, name, n=0):
         sys.exit(n)
 
@@ -48,17 +68,37 @@ class Shell:
         os.chdir(d)
 
 class TokenType(Enum):
+    '''
+    Token types that are recognized by the Tokenizer.
+    '''
+
     WORD = enum.auto()
     EOF = enum.auto()
     UNKNOWN = enum.auto()
 
 class Token:
+    '''
+    A string with an assigned meaning.
+
+    Args:
+        ttype: The token meaning.
+        lexeme: The token value (optional).
+        position: The location of the token in the stream.
+    '''
+
     def __init__(self, ttype, lexeme=None, position=None):
         self.lexeme = lexeme
         self.ttype = ttype
         self.position = position
 
 class Tokenizer:
+    '''
+    Performs lexical analysis on a raw string.
+
+    Args:
+        string: The raw string on which to operate.
+    '''
+
     def __init__(self, string):
         self.string = string
         self.position = -1
@@ -67,6 +107,13 @@ class Tokenizer:
         self.read()
 
     def read(self):
+        '''
+        Read a single char from the stream and store it in self.char.
+
+        Returns:
+            The value of self.char.
+        '''
+
         self.position += 1
         if self.position < len(self.string):
             self.char = self.string[self.position]
@@ -75,6 +122,17 @@ class Tokenizer:
         return self.char
 
     def token(self):
+        '''
+        Read a single token from the stream.
+
+        Returns:
+            The generated token.
+
+        Throws:
+            May throw a ValueError in the case that the input is malformed and
+            a token cannot be correctly generated from it.
+        '''
+
         # ignore whitespace
         while self.char and self.char.isspace():
             self.read()
@@ -93,7 +151,7 @@ class Tokenizer:
                 value.append(self.char)
                 self.read()
             if self.char is None:
-                raise ValueError('unexpected end of line')
+                raise ValueError('unexpected end of line while reading quoted word')
             else:
                 self.read()
 
@@ -114,6 +172,10 @@ class Tokenizer:
             return token
 
     def __iter__(self):
+        '''
+        Utility iterator to allow easy creation of a stream of tokens.
+        '''
+
         while True:
             token = self.token()
             yield token
@@ -121,6 +183,13 @@ class Tokenizer:
             if token.ttype == TokenType.EOF: break
 
 class Parser:
+    '''
+    Parses a stream of tokens into an Abstract Syntax Tree for later execution.
+
+    Args:
+        tokens: The stream of tokens.
+    '''
+
     def __init__(self, tokens):
         self.tokens = iter(tokens)
         self.token = None
@@ -129,6 +198,17 @@ class Parser:
         self.next()
 
     def parse(self):
+        '''
+        Parse the stream of tokens.
+
+        Returns:
+            The root node of the Abstract Syntax Tree.
+
+        Throws:
+            May throw a ValueError in the case that the stream of tokens is
+            malformed.
+        '''
+
         return self.command()
 
     def command(self):
@@ -165,13 +245,32 @@ class Parser:
             raise ValueError(f'expected token to be {ttype}, instead got {self.token.ttype}')
 
 class Node:
+    '''
+    A single node in the Abstract Syntax Tree.
+    '''
+
     def execute(self, builtins):
+        '''
+        Execute the node.
+
+        Args:
+            builtins: A dict of builtin commands.
+        '''
+
         pass
 
     def wait(self):
+        '''
+        Wait for the execution of the node to complete.
+        '''
+
         pass
 
 class CommandNode:
+    '''
+    A node that contains a single shell command.
+    '''
+
     def __init__(self, command, args):
         self.command = command
 
